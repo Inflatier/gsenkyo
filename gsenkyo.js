a508cc26fbb36ca916b7d02822b6df1ef1a093b1
猪野 湧斗 <ice.softicon30th@gmail.com>
2016-01-16 15:19:11 +0900

    ROOMが表示される

--- gsenkyo.js
+++ gsenkyo.js
@@ -6,8 +6,31 @@ db.rooms = new Mongo.Collection('rooms');
 if (Meteor.isClient) {
   Session.setDefault('creating-room', false);
   Session.setDefault('selecting-room', false);
+  Session.setDefault('entrying', false);
+  Session.setDefault('selectedRoom', null);
+  Session.setDefault('password', null);
+  Session.setDefault('logged-in', false);
+
+  // データベースにアクセスできるようにする
   Meteor.subscribe('rooms');
   Meteor.subscribe('logs');
+
+  /**
+  * テンプレート内の"{{ヘルパー名}}"でアクセスできる値を設定する
+  *
+  * Template.テンプレート名.helpers({
+  *    ヘルパー名: 値
+  *  })
+  *
+  *
+  * テンプレート内で起きたイベントを処理する
+  *
+  * Template.テンプレート名.events({
+  *    'イベント名 CSSセレクタ': 関数(event, template) {
+  *      template.find('#CSSセレクタ')
+  *    }
+  *  })
+  */
 
   Template.body.helpers({
     creatingRoom: function () {
@@ -15,8 +38,12 @@ if (Meteor.isClient) {
     },
     selectingRoom: function () {
       return Session.get('selecting-room');
+    },
+    loggedIn: function () {
+      return Session.get('logged-in');
     }
   });
+
 
   Template.title.events({
     'click button.join': function () {
@@ -33,6 +60,7 @@ if (Meteor.isClient) {
     }
   });
 
+
   Template.createRoom.events({
     'submit form': function (e, template) {
       e.preventDefault();
@@ -43,10 +71,40 @@ if (Meteor.isClient) {
     }
   });
 
+
   Template.selectRoom.events({
+    /**
+    * 部屋が選択されたら
+    */
     'click li': function (e, template) {
       Session.set('entrying', true);
       Session.set('selectedRoom', this._id);
+    },
+
+    /**
+    * '参加'の処理
+    */
+    'submit form': function (e, template) {
+      // 画面のリロードを防ぐ
+      e.preventDefault();
+
+      var roomId = Session.get('selectedRoom');
+      var name = template.find('input.name').value;
+      var password = template.find('input.password').value;
+
+      // Meteor.methodsで定義したメソッドloginを呼び出し
+      Meteor.call('login', roomId, name, password, function (err) {
+        if (err) {
+          // ログインできなかった
+          console.error(err);
+        } else {
+          // ログインできた
+          Session.setPersistent('logged-in', true);
+          Session.setPersistent('password', password);
+          Session.set('selecting-room', false);
+          Session.set('entrying', false);
+        }
+      });
     },
     'click .cancel': function () {
       Session.set('entrying', false);
@@ -55,32 +113,61 @@ if (Meteor.isClient) {
 
   Template.selectRoom.helpers({
     rooms: function () {
-      return db.rooms.find({}, {name: 1});
+      return db.rooms.find({}, {name: 1, password: 0});
     },
     entrying: function () {
       return (Session.get('entrying')) ? 'entrying' : '';
     },
     room: function () {
-      var room = db.rooms.findOne(
-        {_id: Session.get('selectedRoom')});
+      var roomId = Session.get('selectedRoom');
+      var room = db.rooms.findOne({_id: roomId});
       return (room) ? room.name : '';
     }
   });
+
 }
 
 if (Meteor.isServer) {
+
+  // サーバー起動時に挿入されるサンプルデータ
   Meteor.startup(function () {
     db.logs.update({}, {name: 'ここにログが入る予定'}, {upsert: true});
-    db.rooms.update({}, {name: '部屋の名前', password: 'PASSWORD_NO_JAPANESE'}, {upsert: true});
+    db.rooms.update({}, {name: '部屋の名前', password: 'pass'}, {upsert: true});
   });
 
-db.rooms.allow({
-  insert: function (userId, doc) {
-    if (!doc.name) return false;
-    if (!doc.password) return false;
-    return true;
-  }
-});
+  // name, passwordがあるデータだけが保存できるようにする
+  db.rooms.allow({
+    insert: function (userId, doc) {
+      if (!doc.name) return false;
+      if (!doc.password) return false;
+      return true;
+    }
+  });
+
+  // Meteor.callで呼び出すためのメソッド
+  Meteor.methods({
+    login: function (roomId, name, password) {
+      var room = db.rooms.findOne({_id: roomId});
+      // 引数の型チェック
+      check(roomId, String);
+      check(name, String);
+      check(password, String);
+      check(room.password, String);
+
+      // 名前が0文字ならエラー
+      if (name.length == 0) {
+        throw new Meteor.Error('Input name');
+      }
+
+      if (room.password === password) {
+        return 'Login successful';
+      } else {
+        throw new Meteor.Error('Wrong password')
+      }
+    }
+  });
+
+  // クライアントにデータベースを公開する
 
   Meteor.publish('rooms', function () {
     return db.rooms.find({});
@@ -89,4 +176,5 @@ db.rooms.allow({
   Meteor.publish('logs', function () {
     return db.logs.find({});
   });
+
 }
