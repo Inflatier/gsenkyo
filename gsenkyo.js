777c2d3f2a1e336d7cbefa1d37e84b41dcafb1c4
猪野 湧斗 <ice.softicon30th@gmail.com>
2016-01-16 19:09:39 +0900

    参加人数を消した

--- gsenkyo.js
+++ gsenkyo.js
@@ -79,6 +79,9 @@ if (Meteor.isClient) {
       var password = template.find('input.password').value;
       db.rooms.insert({name: name, password: password});
       Session.set('creating-room', false);
+    },
+    'click .cancel': function (e, template) {
+      Session.set('creating-room', false);
     }
   });
 
@@ -165,7 +168,7 @@ if (Meteor.isServer) {
 		password: 'pass',
 		players:[],
 	}, {upsert: true});
-	
+
   });
 
   // name, passwordがあるデータだけが保存できるようにする
